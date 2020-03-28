117eafc485ad3c83be8f65cdce67d2197bc698b6
agritak <60624403+agritak@users.noreply.github.com>
2020-03-28 12:21:11 +0200

    Initial commit

---
+++ src/main/java/spring/movieclinic/MovieclinicApplication.java
@@ -0,0 +1,13 @@
+package spring.movieclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/movieclinic/MovieclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.movieclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
