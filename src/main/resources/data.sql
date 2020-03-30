535a266692b0be83afd090e0f72f562d3cfb2486
IrinaTkacenko <irinatkacenko@inbox.lv>
2020-03-30 20:55:33 +0300

    data.sql categories scripts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,11 @@
 INSERT INTO movies
 VALUES (null,
 	"Snatch",
-	"Unscrupulous boxing promoters, violent bookmakers, a Russian gangster,incompetent amateur robbers and supposedly Jewish jewelers fight to track down a priceless stolen diamond.",
+	"Unscrupulous boxing promoters, violent bookmakers, a Russian gangster,incompetent amateur robbers and supposedly
+	 Jewish jewelers fight to track down a priceless stolen diamond.",
 	2000,
-    "https://m.media-amazon.com/images/M/MV5BMTA2NDYxOGYtYjU1Mi00Y2QzLTgxMTQtMWI1MGI0ZGQ5MmU4XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SY1000_SX684_AL_.jpg",
+    "https://m.media-amazon.com/images/M/MV5BMTA2NDYxOGYtYjU1Mi00Y2QzLTgxMTQtMWI1MGI0ZGQ5MmU4XkEy
+    XkFqcGdeQXVyNDk3NzU2MTQ@._V1_SY1000_SX684_AL_.jpg",
     "https://www.imdb.com/video/vi1558577433?playlistId=tt0208092"
     );
 
@@ -216,3 +218,133 @@ VALUES (null,
     FqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,672,1000_AL_.jpg",
     "https://www.imdb.com/video/vi607099161?playlistId=tt0054215"
     );
+
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Action",
+    	"A film genre in which the protagonist or protagonists are thrust into a
+        series of events that typically include violence, extended fighting, physical feats,
+        and frantic chases. Action films tend to feature a resourceful hero struggling against
+        incredible odds, which include life-threatening situations, a villain, or a pursuit
+        which usually concludes in victory for the hero. Genre began to develop
+        in the 1970s along with the increase of stunts and special effects.",
+        "https://m.media-amazon.com/images/M/MV5BZTA3YTUyOTQtMzY0OC00NWU1LTgzOWMtMmNjNGVlZThlZj
+        A3XkEyXkFqcGdeQXVyOTc5MDI5NjE@._V1_SX1777_CR0,0,1777,999_AL_.jpg"
+        );
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Adventure",
+    	"Adventure films typically use their action scenes to display and explore exotic
+        locations in an energetic way. Main plot elements include quests for lost continents
+        and exotic setting; struggles and situations that confront the main characters,
+        the creation of empires, characters embarking on treasure and heroic journeys, travels,
+        explorations, quests and searches for the unknown usually also having to overcome
+        an adversary. ",
+        "https://m.media-amazon.com/images/M/MV5BMjkyNTEwNTU0NF5BMl5BanBnXkFtZTcwNTQxNDczMw@@.
+        _V1_SY1000_CR0,0,659,1000_AL_.jpg"
+        );
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Animation",
+    	"Animation films are animated feature films. In traditional animation, images are drawn
+        or painted by hand on transparent celluloid sheets to be photographed and exhibited on film.
+        Today, most animations are made with computer-generated imagery (CGI). While animation
+        films cover and refer to all sorts of different topics, tackling some serious themes
+        and ideas in their stories, on the whole are mainly marketed towards children and
+        younger age demographics.",
+        "https://m.media-amazon.com/images/M/MV5BMTMyODc0ODI2OF5BMl5BanBnXkFtZTcwODQ5OTIzMw@@.
+        _V1_SY1000_CR0,0,1773,1000_AL_.jpg"
+        );
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Comedy",
+    	"A genre of films that is generally intended to be humorous or amusing by inducing laughter.",
+        "https://m.media-amazon.com/images/M/MV5BMTM4OTM0ODU1Ml5BMl5BanBnXkFtZTcwODY3NzE5Ng@@
+        ._V1_SY1000_CR0,0,1359,1000_AL_.jpg"
+        );
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Crime",
+    	"A film genre inspired by and analogous to the crime fiction literary genre. Films of this
+        genre generally involve various aspects of crime and its detection.",
+        "https://m.media-amazon.com/images/M/MV5BMTUzNjMxNjUwN15BMl5BanBnXkFtZTcwNDgxMjIyNw@@.
+        _V1_SY1000_CR0,0,1503,1000_AL_.jpg"
+        );
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Documantary",
+    	"A non-fictional, motion picture intended to \"document reality, primarily for the purposes
+        of instruction, education, or maintaining a historical record\". Documentary has been
+        described as \"a filmmaking practice, a cinematic tradition, and mode of audience
+        reception that is continually evolving and is without clear boundaries\".",
+        "https://m.media-amazon.com/images/M/MV5BMjAyODU1NzM0NF5BMl5BanBnXkFtZTcwNjI1MjYyMQ@@.
+        _V1_.jpg"
+        );
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Drama",
+    	"A genre of narrative fiction (or semi-fiction) intended to be more serious than humorous
+        in tone.",
+        "https://m.media-amazon.com/images/M/MV5BOTQ5MzAyNTc2MV5BMl5BanBnXkFtZTcwNjk1MDYyNw@@.
+        _V1_SY1000_CR0,0,1345,1000_AL_.jpg"
+        );
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Fantasy",
+    	"A genre with fantastic themes, usually magic, supernatural events, mythology, folklore,
+        or exotic fantasy worlds. Fantasy films often have an element of magic, myth, wonder,
+        escapism, and the extraordinary.",
+        "https://m.media-amazon.com/images/M/MV5BMTc5NTU2NDQ5Ml5BMl5BanBnXkFtZTcwMDIxMjk2Mw@@.
+        _V1_SY1000_CR0,0,1767,1000_AL_.jpg"
+        );
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Family",
+    	"Films of different genres that are designed for watching with the whole family.",
+        "https://m.media-amazon.com/images/M/MV5BNDg1MDY4MzI4OF5BMl5BanBnXkFtZTcwMDQ2ODkyMw@@.
+        _V1_SX1777_CR0,0,1777,740_AL_.jpg"
+        );
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Horror",
+    	"A film that seeks to elicit fear for entertainment purposes. Horror films often aim
+        to evoke viewers' nightmares, fears, revulsions and terror of the unknown. Plots within
+        the horror genre often involve the intrusion of an evil force, event, or personage
+        into the everyday world. Initially inspired by literature from authors like Edgar Allan Poe,
+        Bram Stoker, and Mary Shelley, horror has existed as a film genre for more than a century.",
+        "https://m.media-amazon.com/images/M/MV5BMTg0MzkzODUwNV5BMl5BanBnXkFtZTgwODM1MjEwNDI@.
+        _V1_SX1252_CR0,0,1252,999_AL_.jpg"
+        );
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Sci-Fi",
+    	"Science fiction (sometimes called sci-fi or just SF) is a genre of speculative fiction that
+        typically deals with imaginative and futuristic concepts such as advanced science and
+        technology, space exploration, time travel, parallel universes, and extraterrestrial life.",
+        "https://m.media-amazon.com/images/M/MV5BNDkxNzY5MjIxNF5BMl5BanBnXkFtZTgwNDg5NjUyMjI@.
+        _V1_SX1777_CR0,0,1777,744_AL_.jpg"
+        );
+
+    INSERT INTO categories
+    VALUES (null,
+    	"Thriller",
+    	"Also known as suspense film, is a broad film genre that evokes
+        excitement and suspense in the audience. Tension is created by delaying what the audience
+        sees as inevitable, and is built through situations that are menacing or where escape
+        seems impossible. Thriller films' characters conflict with each other or with an outside
+        force, which can sometimes be abstract. The protagonist is usually set against a problem,
+        such as an escape, a mission, or a mystery.",
+        "https://m.media-amazon.com/images/M/MV5BZjJjYjA0ODctYmQyZi00ZWIyLWE4M2ItNTNhZDU3NzE1OWM2
+        XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SY1000_CR0,0,1503,1000_AL_.jpg"
+        );
