4e0ad004404d31fd090fedb2e2d9512de42692b1
IrinaTkacenko <irinatkacenko@inbox.lv>
2020-03-30 21:01:00 +0300

    data.sql categories scripts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -348,3 +348,73 @@ VALUES (null,
         "https://m.media-amazon.com/images/M/MV5BZjJjYjA0ODctYmQyZi00ZWIyLWE4M2ItNTNhZDU3NzE1OWM2
         XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SY1000_CR0,0,1503,1000_AL_.jpg"
         );
+
+    INSERT INTO movie_category VALUES ( 1, 4);
+    INSERT INTO movie_category VALUES ( 1, 5);
+
+    INSERT INTO movie_category VALUES ( 2, 1);
+    INSERT INTO movie_category VALUES ( 2, 2);
+    INSERT INTO movie_category VALUES ( 2, 4);
+
+    INSERT INTO movie_category VALUES ( 3, 4);
+    INSERT INTO movie_category VALUES ( 3, 5);
+
+    INSERT INTO movie_category VALUES ( 4, 1);
+    INSERT INTO movie_category VALUES ( 4, 2);
+    INSERT INTO movie_category VALUES ( 4, 11);
+
+    INSERT INTO movie_category VALUES ( 5, 12);
+    INSERT INTO movie_category VALUES ( 5, 5);
+    INSERT INTO movie_category VALUES ( 5, 7);
+
+    INSERT INTO movie_category VALUES ( 6, 7);
+    INSERT INTO movie_category VALUES ( 6, 4);
+
+    INSERT INTO movie_category VALUES ( 7, 1);
+    INSERT INTO movie_category VALUES ( 7, 4);
+    INSERT INTO movie_category VALUES ( 7, 5);
+
+    INSERT INTO movie_category VALUES ( 8, 7);
+
+    INSERT INTO movie_category VALUES ( 9, 5);
+    INSERT INTO movie_category VALUES ( 9, 7);
+
+    INSERT INTO movie_category VALUES ( 10, 5);
+    INSERT INTO movie_category VALUES ( 10, 7);
+
+    INSERT INTO movie_category VALUES ( 11, 2);
+    INSERT INTO movie_category VALUES ( 11, 7);
+    INSERT INTO movie_category VALUES ( 11, 11);
+
+    INSERT INTO movie_category VALUES ( 12, 1);
+    INSERT INTO movie_category VALUES ( 12, 11);
+
+    INSERT INTO movie_category VALUES ( 13, 5);
+    INSERT INTO movie_category VALUES ( 13, 7);
+    INSERT INTO movie_category VALUES ( 13, 8);
+
+    INSERT INTO movie_category VALUES ( 14, 1);
+    INSERT INTO movie_category VALUES ( 14, 2);
+    INSERT INTO movie_category VALUES ( 14, 8);
+
+    INSERT INTO movie_category VALUES ( 15, 3);
+    INSERT INTO movie_category VALUES ( 15, 2);
+    INSERT INTO movie_category VALUES ( 15, 9);
+
+    INSERT INTO movie_category VALUES ( 16, 5);
+    INSERT INTO movie_category VALUES ( 16, 7);
+    INSERT INTO movie_category VALUES ( 16, 12);
+
+    INSERT INTO movie_category VALUES ( 17, 4);
+    INSERT INTO movie_category VALUES ( 17, 7);
+
+    INSERT INTO movie_category VALUES ( 18, 1);
+    INSERT INTO movie_category VALUES ( 18, 2);
+    INSERT INTO movie_category VALUES ( 18, 4);
+
+    INSERT INTO movie_category VALUES ( 19, 3);
+    INSERT INTO movie_category VALUES ( 19, 2);
+    INSERT INTO movie_category VALUES ( 19, 9);
+
+    INSERT INTO movie_category VALUES ( 20, 10);
+    INSERT INTO movie_category VALUES ( 20, 12);
