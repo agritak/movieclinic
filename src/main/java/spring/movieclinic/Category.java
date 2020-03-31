0bcdd8dec183f3718080cadefdd8ef10463d17d4
rozenbaumal <rozenbauma.l@gmail.com>
2020-03-31 19:45:51 +0100

    category class_updated but not final

--- src/main/java/spring/movieclinic/Category.java
+++ src/main/java/spring/movieclinic/Category.java
@@ -1,47 +1,53 @@
 package spring.movieclinic;
 
-import java.awt.image.BufferedImage;
+import lombok.Data;
+import spring.movieclinic.movie.Movie;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "categories")
 public class Category {
 
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
     private String name;
+
+    @Size(max = 250, message = ("The maximum length of the description is 250 symbols."))
     private String description;
-    private BufferedImage image;
 
-    public String getId() {
-        return id;
+    @Column(name = "picture_url")
+    private String pictureURL;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Movie> movies;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) &&
+                Objects.equals(name, category.name) &&
+                Objects.equals(description, category.description) &&
+                Objects.equals(pictureURL, category.pictureURL) &&
+                Objects.equals(movies, category.movies);
     }
 
-    public void setId(String id) {
-        this.id = id;
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, pictureURL, movies);
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BufferedImage getImage() {
-        return image;
-    }
-
-    public void setImage(BufferedImage image) {
-        this.image = image;
-    }
-
-    //    public void setImage(BufferedImage image) throws IOException {
-//        this.image = ImageIO.read(new File(name + ".png"));
-//    }
 }
+
+
