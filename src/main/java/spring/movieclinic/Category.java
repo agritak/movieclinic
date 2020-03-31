9745130ac0b64d8613de023c85a4b366d87c302d
rozenbaumal <rozenbauma.l@gmail.com>
2020-03-31 17:25:30 +0100

    category class_draft

--- src/main/java/spring/movieclinic/Category.java
+++ src/main/java/spring/movieclinic/Category.java
@@ -1,50 +1,47 @@
 package spring.movieclinic;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 
 public class Category {
 
-        private String id;
-        private String name;
-        private String description;
-        private BufferedImage image;
+    private String id;
+    private String name;
+    private String description;
+    private BufferedImage image;
 
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public BufferedImage getImage() {
-            return image;
-        }
-
-        public void setImage(BufferedImage image) {
-            this.image = image;
-        }
-
-        //    public void setImage(BufferedImage image) throws IOException {
-//        this.image = ImageIO.read(new File(name + ".png"));
-//    }
+    public String getId() {
+        return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    //    public void setImage(BufferedImage image) throws IOException {
+//        this.image = ImageIO.read(new File(name + ".png"));
+//    }
+}
