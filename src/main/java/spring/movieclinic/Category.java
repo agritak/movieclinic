a21b638484091371cbcaecade01c6b463030cb04
rozenbaumal <rozenbauma.l@gmail.com>
2020-04-01 09:45:05 +0100

    category class_updated but not final

--- src/main/java/spring/movieclinic/Category.java
+++ src/main/java/spring/movieclinic/Category.java
@@ -39,6 +39,38 @@ public class Category {
         this.description = description;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPictureURL() {
+        return pictureURL;
+    }
+
+    public void setPictureURL(String pictureURL) {
+        this.pictureURL = pictureURL;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
