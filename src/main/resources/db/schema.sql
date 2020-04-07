71d691d8bfa626c3b6621e20392a20bf7d276de8
agritak <60624403+agritak@users.noreply.github.com>
2020-04-07 19:03:20 +0300

    Unique keys for tables (movies, categories)

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,22 +1,22 @@
-CREATE TABLE movies(
+CREATE TABLE IF NOT EXISTS movies(
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(200),
+    name VARCHAR(200) NOT NULL,
 	description VARCHAR(500),
-    year INT,
+    year INT NOT NULL,
     picture_url VARCHAR(200),
-    trailer_url VARCHAR(200)
+    trailer_url VARCHAR(200),
+    UNIQUE KEY uk_movies (name, year)
     );
 
-CREATE TABLE categories(
+CREATE TABLE IF NOT EXISTS categories(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
+    name VARCHAR(100) NOT NULL,
     description VARCHAR(500),
-    picture_url VARCHAR(250)
+    picture_url VARCHAR(250),
+    UNIQUE KEY uk_categories (name)
     );
 
-CREATE TABLE movie_category(
-    movie_id INT NOT NULL,
-    category_id INT NOT NULL,
-    FOREIGN KEY (movie_id) REFERENCES movies(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id)
+CREATE TABLE IF NOT EXISTS movie_category(
+    movie_id INT,
+    category_id INT
     );
