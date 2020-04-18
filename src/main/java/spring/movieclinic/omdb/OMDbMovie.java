665de2ec231b812460463346f472a4080290f218
agritak <60624403+agritak@users.noreply.github.com>
2020-04-18 14:36:53 +0300

    OMDbMovie

---
+++ src/main/java/spring/movieclinic/omdb/OMDbMovie.java
@@ -0,0 +1,25 @@
+package spring.movieclinic.omdb;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class OMDbMovie {
+    @JsonProperty("imdbID")
+    private String id;
+    @JsonProperty("Title")
+    private String name;
+    @JsonProperty("Plot")
+    private String description;
+    @JsonProperty("Year")
+    private String year;
+    @JsonProperty("Genre")
+    private String categories;
+    @JsonProperty("Poster")
+    private String pictureURL;
+    @JsonProperty("Type")
+    private String type;
+
+}
