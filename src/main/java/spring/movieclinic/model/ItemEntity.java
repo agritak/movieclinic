cc5f1c89ebd611a0bec047d25802edd02a069a46
agritak <60624403+agritak@users.noreply.github.com>
2020-04-19 20:17:55 +0300

    Validation removed from ItemEntity

--- src/main/java/spring/movieclinic/model/ItemEntity.java
+++ src/main/java/spring/movieclinic/model/ItemEntity.java
@@ -10,8 +10,6 @@ import org.hibernate.search.annotations.*;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -28,13 +26,11 @@ public class ItemEntity extends BaseEntity {
 
     @Field
     @Analyzer(definition = "movieAnalyzer")
-    @NotBlank
     @Column(unique = true)
     private String name;
 
     @Field
     @Analyzer(definition = "movieAnalyzer")
-    @Size(max = 500, message = "Maximum length of description is 500 symbols.")
     private String description;
 
     @Column(name = "picture_url")
