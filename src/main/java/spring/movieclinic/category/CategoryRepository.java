727f5d41b7c7784faba7429d9192b6bc1ce97c68
agritak <60624403+agritak@users.noreply.github.com>
2020-04-19 20:42:09 +0300

    CategoryRepository: method findByName(String name)

--- src/main/java/spring/movieclinic/category/CategoryRepository.java
+++ src/main/java/spring/movieclinic/category/CategoryRepository.java
@@ -1,13 +1,14 @@
 package spring.movieclinic.category;
 
 import org.springframework.data.repository.CrudRepository;
-import spring.movieclinic.category.Category;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends CrudRepository <Category, Integer> {
     List<Category> findByNameContains(String name);
 
     List<Category> findByOrderByNameAsc();
 
+    Optional<Category> findByName(String name);
 }
