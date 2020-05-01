2eddbb3bc719c7e06b21118478062b067e2dc1f5
IrinaTkacenko <irinatkacenko@inbox.lv>
2020-05-01 13:35:33 +0300

    Added necessary findBy methods.

--- src/main/java/spring/movieclinic/movie/MovieRepository.java
+++ src/main/java/spring/movieclinic/movie/MovieRepository.java
@@ -1,11 +1,20 @@
 package spring.movieclinic.movie;
 
 import org.springframework.data.repository.CrudRepository;
+import spring.movieclinic.category.Category;
 
 import java.util.List;
+import java.util.Set;
 
 public interface MovieRepository extends CrudRepository<Movie, Integer> {
+
     List<Movie> findByNameContains(String name);
+
+    List<Movie> findByYear(Integer year);
+
+    List<Movie> findByCategoriesIn(Set<Category> categories);
+
+    List<Movie> findByDescriptionContains(String description);
 
     List<Movie> findByOrderByNameAsc();
 
