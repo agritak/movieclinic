d4c516aac9480bd3bdc0824b1df8708228008c10
agritak <60624403+agritak@users.noreply.github.com>
2020-05-11 19:21:11 +0300

    exception handling and movies-validation

--- src/main/java/spring/movieclinic/category/CategoryRepository.java
+++ src/main/java/spring/movieclinic/category/CategoryRepository.java
@@ -4,11 +4,16 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
-public interface CategoryRepository extends CrudRepository <Category, Integer> {
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
     List<Category> findByNameContains(String name);
 
     List<Category> findByOrderByNameAsc();
 
     Optional<Category> findByName(String name);
+
+    Set<Category> findByNameIn(List<String> names);
+
+    Set<Category> findByIdIn(Set<Integer> ids);
 }
--- src/main/java/spring/movieclinic/movie/MoviesService.java
+++ src/main/java/spring/movieclinic/movie/MoviesService.java
@@ -2,18 +2,32 @@ package spring.movieclinic.movie;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import spring.movieclinic.category.CategoryRepository;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class MoviesService {
 
     private final MovieRepository movieRepository;
+    private final CategoryRepository categoryRepository;
 
     public List<Movie> movies() {
         return movieRepository.findByOrderByNameAsc();
+    }
+
+    public List<Movie> moviesShuffled() {
+        List<Movie> movies = (List<Movie>) movieRepository.findAll();
+        Collections.shuffle(movies);
+        return movies;
+    }
+
+    public Movie findMovie(Integer id) {
+        return movieRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid movie Id:" + id));
     }
 
     void create(FrontMovie frontMovie) {
@@ -21,31 +35,26 @@ public class MoviesService {
     }
 
     void update(Integer id, FrontMovie frontMovie) {
-        movieRepository.save(frontMovieToMovie(frontMovie, findById(id)));
+        movieRepository.save(frontMovieToMovie(frontMovie, findMovie(id)));
     }
 
     void delete(Integer id) {
-        movieRepository.deleteById(id);
+        Optional<Movie> delete = movieRepository.findById(id);
+        delete.ifPresent(movieRepository::delete);
+        //movieRepository.deleteById(id);
     }
 
-    public Movie findById(Integer id) {
-        return movieRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid movie Id:" + id));
+    Optional<Movie> movieExists(String name, Integer year) {
+        return movieRepository.findByNameAndYear(name, year);
     }
 
-    public List<Movie> moviesShuffled() {
-        List<Movie> movies = movies();
-        Collections.shuffle(movies);
-        return movies;
-    }
-
-    private Movie frontMovieToMovie(FrontMovie frontMovie, Movie movie) {
+    Movie frontMovieToMovie(FrontMovie frontMovie, Movie movie) {
         movie.setName(frontMovie.getName());
         movie.setDescription(frontMovie.getDescription());
         movie.setYear(frontMovie.getYear());
         movie.setPictureURL(frontMovie.getPictureURL());
         movie.setTrailerURL(frontMovie.getTrailerURL());
-        movie.setCategories(frontMovie.getCategories());
+        movie.setCategories(categoryRepository.findByIdIn(frontMovie.getCategories()));
         return movie;
     }
 }
