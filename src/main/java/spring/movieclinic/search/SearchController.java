62a329a0a87dd3f21d6f8a7e3f8b79ace9549d98
IrinaTkacenko <irinatkacenko@inbox.lv>
2020-05-21 18:38:50 +0300

    Added pagination admin side search results.

--- src/main/java/spring/movieclinic/search/SearchController.java
+++ src/main/java/spring/movieclinic/search/SearchController.java
@@ -1,12 +1,14 @@
 package spring.movieclinic.search;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import spring.movieclinic.category.CategoriesService;
 import spring.movieclinic.movie.Movie;
+import spring.movieclinic.movie.MoviesService;
 
 @Controller
 @AllArgsConstructor
@@ -14,34 +16,31 @@ class SearchController {
 
     private final CategoriesService categoriesService;
     private final SearchService searchService;
+    private final MoviesService moviesService;
 
     @GetMapping("admin/search/search-results")
-    String adminSideSearch(@RequestParam(
-            value = "search") final String query,
-                                final Model model) {
+    String adminSideSearch(@RequestParam(defaultValue = "1") Integer page,
+                           @RequestParam(defaultValue = "5") Integer size,
+                           @RequestParam(value = "search") String query,
+                           Model model) {
         model.addAttribute("query", query);
-        model.addAttribute("searchResults",
-                searchService.getSearchBarResults(query));
+        model.addAttribute("paging", moviesService.paginateAnyMoviesList(
+                PageRequest.of(page - 1, size),
+                searchService.getSearchBarResults(query)));
         return "search/search-results";
     }
 
     @GetMapping("/search/user-search-results")
-    String userSideSearch(@RequestParam(
-            value = "search", required = false) final String query,
-                                                final Movie movie,
-                                                final Model model) {
+    String userSideSearch(@RequestParam(value = "search", required = false) String query, Movie movie, Model model) {
         model.addAttribute("options", categoriesService.categories());
-        model.addAttribute("query",
-                searchService.getQueryToDisplay(query, movie));
-        model.addAttribute("searchResults",
-                searchService.getUserSearchResults(query, movie));
+        model.addAttribute("query", searchService.getQueryToDisplay(query, movie));
+        model.addAttribute("searchResults", searchService.getUserSearchResults(query, movie));
         return "search/user-search-results";
     }
 
     @GetMapping("/advanced")
-    String showAdvancedSearchForm(final Model model) {
-        model.addAttribute("options",
-                categoriesService.categories());
+    String showAdvancedSearchForm(Model model) {
+        model.addAttribute("options", categoriesService.categories());
         model.addAttribute("movie", new Movie());
         return "search/advanced-search";
     }
