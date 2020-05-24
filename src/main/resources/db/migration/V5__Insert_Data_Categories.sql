5c723bb2a1b65323291bbb3186cb864442bc5574
IrinaTkacenko <irinatkacenko@inbox.lv>
2020-05-24 15:52:03 +0300

    Corrected descriptions.

--- src/main/resources/db/migration/V5__Insert_Data_Categories.sql
+++ src/main/resources/db/migration/V5__Insert_Data_Categories.sql
@@ -1,30 +1,19 @@
 INSERT INTO categories
     VALUES (null,
     	'Action',
-    	'A film genre in which the protagonist or protagonists are thrust into a
-        series of events that typically include violence, extended fighting, physical feats,
-        and frantic chases. Action films tend to feature a resourceful hero struggling against
-        incredible odds, which include life-threatening situations, a villain, or a pursuit
-        which usually concludes in victory for the hero.'
+    	'A film genre in which the protagonist or protagonists are thrust into a series of events that typically include violence, extended fighting, physical feats, and frantic chases. Action films tend to feature a resourceful hero struggling against incredible odds, which include life-threatening situations, a villain, or a pursuit which usually concludes in victory for the hero.'
         );
 
     INSERT INTO categories
     VALUES (null,
     	'Adventure',
-    	'Adventure films typically use their action scenes to display and explore exotic
-        locations in an energetic way. Main plot elements include quests for lost continents
-        and exotic setting; struggles and situations that confront the main characters,
-        the creation of empires, characters embarking on treasure and heroic journeys, travels,
-        explorations, quests and searches for the unknown usually also having to overcome
-        an adversary. '
+    	'Adventure films typically use their action scenes to display and explore exotic locations in an energetic way. Main plot elements include quests for lost continents and exotic setting; struggles and situations that confront the main characters, the creation of empires, characters embarking on treasure and heroic journeys, travels, explorations, quests and searches for the unknown usually also having to overcome an adversary. '
         );
 
     INSERT INTO categories
     VALUES (null,
     	'Animation',
-    	'Animation films are animated feature films. In traditional animation, images are drawn
-        or painted by hand on transparent celluloid sheets to be photographed and exhibited on film.
-        Today most animations are made with computer-generated imagery (CGI).'
+    	'Animation films are animated feature films. In traditional animation, images are drawn or painted by hand on transparent celluloid sheets to be photographed and exhibited on film. Today most animations are made with computer-generated imagery (CGI).'
         );
 
     INSERT INTO categories
@@ -36,32 +25,25 @@ INSERT INTO categories
     INSERT INTO categories
     VALUES (null,
     	'Crime',
-    	'A film genre inspired by and analogous to the crime fiction literary genre. Films of this
-        genre generally involve various aspects of crime and its detection.'
+    	'A film genre inspired by and analogous to the crime fiction literary genre. Films of this genre generally involve various aspects of crime and its detection.'
         );
 
     INSERT INTO categories
     VALUES (null,
     	'Documentary',
-    	'A non-fictional, motion picture intended to \"document reality, primarily for the purposes
-        of instruction, education, or maintaining a historical record\". Documentary has been
-        described as \"a filmmaking practice, a cinematic tradition, and mode of audience
-        reception that is continually evolving and is without clear boundaries\".'
+    	'A non-fictional, motion picture intended to \"document reality, primarily for the purposes of instruction, education, or maintaining a historical record\". Documentary has been described as \"a filmmaking practice, a cinematic tradition, and mode of audience reception that is continually evolving and is without clear boundaries\".'
         );
 
     INSERT INTO categories
     VALUES (null,
     	'Drama',
-    	'A genre of narrative fiction (or semi-fiction) intended to be more serious than humorous
-        in tone.'
+    	'A genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.'
         );
 
     INSERT INTO categories
     VALUES (null,
     	'Fantasy',
-    	'A genre with fantastic themes, usually magic, supernatural events, mythology, folklore,
-        or exotic fantasy worlds. Fantasy films often have an element of magic, myth, wonder,
-        escapism, and the extraordinary.'
+    	'A genre with fantastic themes, usually magic, supernatural events, mythology, folklore, or exotic fantasy worlds. Fantasy films often have an element of magic, myth, wonder, escapism, and the extraordinary.'
         );
 
     INSERT INTO categories
@@ -73,27 +55,17 @@ INSERT INTO categories
     INSERT INTO categories
     VALUES (null,
     	'Horror',
-    	'A film that seeks to elicit fear for entertainment purposes. Horror films often aim
-        to evoke viewers nightmares, fears, revulsions and terror of the unknown. Plots within
-        the horror genre often involve the intrusion of an evil force, event, or personage
-        into the everyday world. Initially inspired by literature from authors like Edgar Allan Poe,
-        Bram Stoker, and Mary Shelley, horror has existed as a film genre for more than a century.'
+    	'A film that seeks to elicit fear for entertainment purposes. Horror films often aim to evoke viewers nightmares, fears, revulsions and terror of the unknown. Plots within the horror genre often involve the intrusion of an evil force, event, or personage into the everyday world. Initially inspired by literature from authors like Edgar Allan Poe, Bram Stoker, and Mary Shelley, horror has existed as a film genre for more than a century.'
         );
 
     INSERT INTO categories
     VALUES (null,
     	'Sci-Fi',
-    	'Science fiction (sometimes called sci-fi or just SF) is a genre of speculative fiction that
-        typically deals with imaginative and futuristic concepts such as advanced science and
-        technology, space exploration, time travel, parallel universes, and extraterrestrial life.'
+    	'Science fiction (sometimes called sci-fi or just SF) is a genre of speculative fiction that typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life.'
         );
 
     INSERT INTO categories
     VALUES (null,
     	'Thriller',
-    	'Also known as suspense film, is a broad film genre that evokes
-        excitement and suspense in the audience. Tension is created by delaying what the audience
-        sees as inevitable, and is built through situations that are menacing or where escape
-        seems impossible. Thriller films characters conflict with each other or with an outside
-        force, which can sometimes be abstract.'
+    	'Also known as suspense film, is a broad film genre that evokes excitement and suspense in the audience. Tension is created by delaying what the audience sees as inevitable, and is built through situations that are menacing or where escape seems impossible. Thriller films characters conflict with each other or with an outside force, which can sometimes be abstract.'
         );
